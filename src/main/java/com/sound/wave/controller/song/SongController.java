3bb63616024444fdd542599e369c1e2e8659bcf2
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-13 22:19:11 +0700

    test validate save song

--- src/main/java/com/sound/wave/controller/song/SongController.java
+++ src/main/java/com/sound/wave/controller/song/SongController.java
@@ -5,7 +5,10 @@ import com.sound.wave.service.song.ISongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -21,7 +24,10 @@ public class SongController {
     }
 
     @PostMapping()
-    public ResponseEntity<Song> saveNewSong(@RequestBody Song song){
-        return new ResponseEntity<>(iSongService.save(song), HttpStatus.OK);
+    public ResponseEntity<Song> saveNewSong(@Valid @RequestBody Song song, BindingResult bindingResult){
+        if (!bindingResult.hasFieldErrors()) {
+            return new ResponseEntity<>(iSongService.save(song), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
