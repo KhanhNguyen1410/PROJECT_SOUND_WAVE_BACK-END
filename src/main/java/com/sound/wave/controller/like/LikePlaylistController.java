e1a75afc98c3d62a94372862e8ef4b80465e9db4
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-22 16:57:33 +0700

    done LikePlaylist

--- src/main/java/com/sound/wave/controller/like/LikePlaylistController.java
+++ src/main/java/com/sound/wave/controller/like/LikePlaylistController.java
@@ -1,10 +1,16 @@
 package com.sound.wave.controller.like;
 
+import com.sound.wave.model.PlayList;
+import com.sound.wave.model.Song;
+import com.sound.wave.model.User;
+import com.sound.wave.model.likePlaylist.LikePlaylist;
+import com.sound.wave.model.likeSong.LikeSong;
 import com.sound.wave.service.like.ILikePlaylistService;
+import com.sound.wave.service.playlist.IPlaylistService;
+import com.sound.wave.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +19,54 @@ import org.springframework.web.bind.annotation.*;
 public class LikePlaylistController {
     @Autowired
     private ILikePlaylistService service;
+    @Autowired
+    private IPlaylistService playlistService;
+    @Autowired
+    private IUserService userService;
+
+
     @GetMapping("/{id}")
     public ResponseEntity<Long> findLikeByPlaylistId(@PathVariable("id") Long id){
         Long number = service.findLikeByPlaylistId(id);
         if (number == null){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(number, HttpStatus.OK);
+    }
+    @PostMapping("/like/{p_id}/{u_id}")
+    public ResponseEntity<LikePlaylist> likeSong(@PathVariable("p_id") long p_id, @PathVariable("u_id") long u_id){
+        LikePlaylist likePlaylist = service.checkLikePlaylist(p_id,u_id);
+        if (likePlaylist == null){
+            LikePlaylist likePlaylist1 = new LikePlaylist();
+            PlayList playList = playlistService.findById(p_id).get();
+            User user = userService.findById(u_id).get();
+            likePlaylist1.setPlayList(playList);
+            likePlaylist1.setUser(user);
+            service.save(likePlaylist1);
+            return  new ResponseEntity<>(likePlaylist1, HttpStatus.OK);
+        }
+        else if (!likePlaylist.isStatus()){
+            likePlaylist.setStatus(true);
+            return new ResponseEntity<>(service.save(likePlaylist), HttpStatus.OK);
+        }else {
+            likePlaylist.setStatus(false);
+            return new ResponseEntity<>(service.save(likePlaylist), HttpStatus.OK);
+
+        }
+    }
+    @GetMapping("/all-like/{id}")
+    public ResponseEntity<Iterable<PlayList>> getAllSongsByUserLike(@PathVariable("id") Long id){
+        Iterable<PlayList> playlists = service.findPlaylistsByUserIdAndStatus(id);
+        if (playlists == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(playlists,HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Long> getLikesByPlaylistId(@PathVariable("id") Long id){
+        Long number = service.findLikeByPlaylistId(id);
+        if (number == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(number, HttpStatus.OK);
     }
--- src/main/java/com/sound/wave/service/like/ILikePlaylistServiceImpl.java
+++ src/main/java/com/sound/wave/service/like/ILikePlaylistServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sound.wave.service.like;
 
+import com.sound.wave.model.PlayList;
 import com.sound.wave.model.likePlaylist.LikePlaylist;
 import com.sound.wave.repository.like.ILikePlaylistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import java.util.Optional;
 @Service
 public class ILikePlaylistServiceImpl implements ILikePlaylistService{
     @Autowired
-    private ILikePlaylistRepository repository;
+    private ILikePlaylistRepository iLikePlaylistRepository;
     @Override
     public Iterable<LikePlaylist> findAll() {
         return null;
@@ -33,6 +34,16 @@ public class ILikePlaylistServiceImpl implements ILikePlaylistService{
 
     @Override
     public Long findLikeByPlaylistId(Long id) {
-        return repository.findLikeByPlaylistId(id);
+        return iLikePlaylistRepository.findLikeByPlaylistId(id);
+    }
+
+    @Override
+    public LikePlaylist checkLikePlaylist(Long p_id, Long u_id) {
+        return iLikePlaylistRepository.checkLikePlaylist(p_id, u_id);
+    }
+
+    @Override
+    public Iterable<PlayList> findPlaylistsByUserIdAndStatus(Long id) {
+        return iLikePlaylistRepository.findPlaylistsByUserIdAndStatus(id);
     }
 }
