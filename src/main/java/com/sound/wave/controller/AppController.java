9dcff02f10bff017016584a7e65ba5c28ceaa4e1
anh.tran (c0720i1) <69283798+theanh111@users.noreply.github.com>
2022-01-20 14:58:33 +0700

    Deploy

---
+++ src/main/java/com/sound/wave/controller/AppController.java
@@ -0,0 +1,14 @@
+package com.sound.wave.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World !!!";
+    }
+}
